b679b74a17dc6a2b9b0c6353fc5f73aceab8daf6
ios <wow5702@naver.com>
2025-03-30 23:36:01 +0900

    feat : CORS 오류 수정 & 에러통합메시지 기능 추가

---
+++ src/main/java/com/skala/stock/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.skala.stock.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 경로에 대해
+                        .allowedOrigins("http://localhost:5173") // Vue 개발 서버
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
