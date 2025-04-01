ccbdf370e556bf196028427f3a836546a051eaf5
ios <60501045+ww5702@users.noreply.github.com>
2025-04-01 09:44:54 +0900

    매수가, 현재가 추가

--- src/main/java/com/skala/stock/domain/PlayerStock.java
+++ src/main/java/com/skala/stock/domain/PlayerStock.java
@@ -1,7 +1,14 @@
 package com.skala.stock.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter @Setter
@@ -21,4 +28,6 @@ public class PlayerStock {
     private Stock stock;
 
     private int quantity;
+
+    private int price; // ✅ 매수 당시 가격 (고정)
 }
